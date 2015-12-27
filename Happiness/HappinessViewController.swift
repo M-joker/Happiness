98fb99e8acd9746e50820b0a50704c6f3be2998a
M-joker <alislwd@icloud.com>
2015-12-27 13:49:18 +0800

    可以用手势的笑脸

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -8,8 +8,49 @@
 
 import UIKit
 
-class HappinessViewController: UIViewController
+class HappinessViewController: UIViewController,FaceViewDataSoure
 {
+    var happiness:Int = 100 {//0代表伤心，100代表开心
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale:CGFloat = 4
+        
+    }
+    
+    @IBAction func changehappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChanged = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChanged != 0 {
+                happiness += happinessChanged
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default:break
 
-   
+        }
+    }
+    
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSoure = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale"))
+        }
+    }
+    
+        private func updateUI() {
+        
+        faceView.setNeedsDisplay()
+    }
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
 }
